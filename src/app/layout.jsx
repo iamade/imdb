9aac5a520a6676c0326d74269d5f3b07c528e529
Adesegun Koiki <kamar3deen@gmail.com>
2023-10-30 13:14:47 +0100

    create the header section

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,8 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head></head>
-      <body className={inter.className}>{children}</body>
+     
+      <body>
+        {/* Header */}
+        <Header/>
+        {/* Navbar */}
+        {children}</body>
     </html>
   )
 }
