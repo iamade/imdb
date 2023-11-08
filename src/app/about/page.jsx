178c5197ef09ee73fa7f033cbb1c5ce8a7a48d5b
Adesegun Koiki <kamar3deen@gmail.com>
2023-11-08 07:25:56 +0100

    create Navbar component

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,8 @@
 export default function About() {
   return (
-    <div className="max-w-6xl mx-auto space-y-4">
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
       <h1 className="text-2xl font-medium text-amber-600">About</h1>
-      <h1>
+     
         <p> Welcome to our movie database website! We are a team of passionate movie
         enthusiasts who have come together to create a one-stop destination for
         all your movie-related needs.
@@ -32,7 +32,7 @@ export default function About() {
         website. Happy browsing!
       </p>
 
-    </h1>
+   
 
     </div>
   );
