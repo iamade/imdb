4a539c7f583885ad77b88f7ae6caee692e64fc96
Adesegun Koiki <kamar3deen@gmail.com>
2023-11-08 06:28:57 +0100

    about page

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 
 export default function DarkModeSwitch() {
   const { systemTheme, theme, setTheme } = useTheme();
-  const [ mounted, setMounted] = useState(false);
+  const [ mounted, setMounted ] = useState(false);
 
   useEffect(() => setMounted(true), []);
 
